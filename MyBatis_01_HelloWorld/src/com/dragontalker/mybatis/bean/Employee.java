4b1a95d4c7d4918b9338c900495d34f49e22aa37
Richard <richard.yang.tong@gmail.com>
2021-07-12 07:39:39 -0400

    Added notes.

--- MyBatis_01_HelloWorld/src/com/dragontalker/mybatis/bean/Employee.java
+++ MyBatis_01_HelloWorld/src/com/dragontalker/mybatis/bean/Employee.java
@@ -6,4 +6,29 @@ public class Employee {
 	private String lastName;
 	private String email;
 	private String gender;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
 }
